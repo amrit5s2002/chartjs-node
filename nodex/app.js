9f3283ad5175fbb94999a5b8b77c154c829ce49b
Guramrit Pal Singh <77382202+amrit5s2002@users.noreply.github.com>
2022-11-26 18:59:37 +0530

    Update app.js

--- nodex/app.js
+++ nodex/app.js
@@ -3,12 +3,12 @@ const path = require('path');
 const mysql = require('mysql');
 const { Chart } = require('chart.js');
 const app = express();
+
+
+
 app.set('view engine', 'pug');
+
 const port = process.env.PORT || 8080;
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'ejs')
-
-
 const dates = [];
 const rank =[];
 var month,dnum
@@ -16,7 +16,7 @@ var month,dnum
 const pool = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '123456',
+  password: 'password',
   database: 'sampledata'
 });
 
@@ -28,15 +28,12 @@ app.get('/', function(req, res){
           for(i=1;i<Object.keys(result).length;i++){
             d = result[i].Date;
             d = String(d);
-            // console.log(typeof(d))
             month = d.substr(4,4);
-            // console.log(month)
             if (month.includes('Oct')){
               month = '10';
-              // console.log(month)
             }
             dnum = d.substr(8,2);
-            // console.log(dnum)
+            
             
 
             dyr = d.substr(11,4);
@@ -49,42 +46,8 @@ app.get('/', function(req, res){
             // console.log(d)
             dates.push(d);
             rank.push(r);
-            
-
-            // console.log('working')
-            // console.log(__dirname)
-            
-            
+              
           };
-
-          
-          // const data = {
-          //   labels: dates,
-          //   datasets: [{
-          //     label: '',
-          //     backgroundColor: 'rgb(255, 99, 132)',
-          //     borderColor: 'rgb(255, 99, 132)',
-          //     data: rank,
-          // }]
-          // };
-          // const config = {
-          //   type: 'line',
-          //   data,
-          //   options: {}
-          // };
-          
-
-          // const myChart = new Chart(
-          //   document.getElementById('myChart'),
-          //   config
-          // );
-
-          // console.log(rank)
-          // console.log(dates)
-          // console.log(rank)
-          // var data = { message1 :dates, message2 :rank };
-          // console.log(dates);
-          // module.exports = {result};
           
           res.render('chart', {
             // title: 'Search Hacker News',
@@ -95,10 +58,6 @@ app.get('/', function(req, res){
     
 });
 });
-
-
-
-
 
 
 
